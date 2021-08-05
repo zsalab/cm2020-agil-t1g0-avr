af030bf6742b13124fae4694732453a79dcc0282
Balazs Toth <zsalab@zettasystem.com>
2021-08-05 17:00:02 +0200

    add data endpoints and first animation

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Required External Modules
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
 
 // App Variables
 const app = express();
 const port = process.env.PORT || "8000";
+
+// variables
+const BREATHS_PATH = 'data/breaths';
 
 // App Configuration
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +25,44 @@ app.get("/", (req, res) => {
 			image: "/breath/tulip/image.jpg",
 		}
 	});
+});
+
+app.get("/breaths", (req, res) => {
+	var retval = [];
+	var files = fs.readdirSync(BREATHS_PATH);
+	files.forEach(file => {
+		var data = fs.readFileSync(BREATHS_PATH+'/'+file+"/settings.json")
+		console.log(file+" "+JSON.parse(data));
+		retval.push(JSON.parse(data));
+	});
+	res.status(200).type('json').send(JSON.stringify(retval));
+});
+
+app.get("/breaths/:id", (req, res) => {
+	var f = BREATHS_PATH+'/'+parseInt(req.params.id)+"/settings.json";
+	if (fs.existsSync(f)) {
+		res.status(200).type('json').send(JSON.stringify(JSON.parse(fs.readFileSync(f))));
+	} else {
+		res.status(404).send("Breath not found");
+	}
+});
+
+app.get("/breaths/:id/image", (req, res) => {
+	var f = BREATHS_PATH+'/'+parseInt(req.params.id)+"/image.jpg";
+	if (fs.existsSync(f)) {
+		res.status(200).type('jpg').send(fs.readFileSync(f));
+	} else {
+		res.status(404).send("Breath image not found");
+	}
+});
+
+app.get("/breaths/:id/animation", (req, res) => {
+	var f = BREATHS_PATH+'/'+parseInt(req.params.id)+"/sketch.js";
+	if (fs.existsSync(f)) {
+		res.status(200).type('js').send(fs.readFileSync(f));
+	} else {
+		res.status(404).send("Breath image not found");
+	}
 });
 
 // Server Activation
