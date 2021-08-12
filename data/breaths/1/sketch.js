06abf2b0e6f6b1c9103fce72abfd22fa7e5cb736
Balazs Toth <zsalab@zettasystem.com>
2021-08-12 21:54:34 +0200

    Add animation clock

--- data/breaths/1/sketch.js
+++ data/breaths/1/sketch.js
@@ -1,81 +1,115 @@
-var diam = 100;
-var inOut = true;
-var breath_counter = 0;
-var num_breath_slider;
-var start = 0;
+var chakraImage;
+var chakraProportion = 0.8;
+var running = false;
+
+var diameter = 0;
+var currentDiameter = 0;
+var minDiameterProportion = 0.05;
+
+var breathingIn = true;
+var breathCount = 0;
+
+var startedAt = 0;
+var animLength = 0;
+var animWidth = 0;
+var midnight = Math.PI*1.5;
 
 function preload() {
-  sarinaFont = loadFont('sarina.regular.ttf');
+	sarinaFont = loadFont('fonts/Sarina-Regular.ttf');
+	chakraImage = loadImage('img/logo-chakra.png')
 }
 
 function setup() {
-    createCanvas(400, 600);
-  
-    num_breath_slider = createSlider(10, 50, 20, 2);
-    num_breath_slider.position(150, 20);
-    num_breath_slider.style('width', '80px');
+	var parent = document.getElementById('breathe')
+	var myCanvas = createCanvas(parent.offsetWidth, parent.offsetHeight)
+	myCanvas.parent(parent.id);
+	animWidth = parent.offsetWidth;
+	currentDiameter = diameter = animWidth;
 }
 
 function draw() {
-  clear();
-    textSize(20);
-    textFont('Georgia');
-    noStroke();
-    fill(157, 183, 184);
-    textSize(15);
-    text("Number of breaths: ", 80, 30);
-    text(num_breath_slider.value(), 250, 30);
-    
-    if (start){
-      textSize(50);
-      fill(157, 183, 184);
-      ellipse(width / 2, (height / 2) * 0.9, diam);
-      breathe();
-    }
-    else{
-      fill(157, 183, 184);
-      ellipse(width / 2, (height / 2) * 0.9, diam*2);
-      textSize(30);
-      textAlign(CENTER, CENTER);
-      textFont(sarinaFont);
-      fill(255,255,255);
-      text("Start", width / 2, height/2 - 30);
-    }
+	clear();
+	noStroke();
+
+	drawGradient()
+	drawChakra();
+	drawText();
+
+	if (running) {
+		breathe();
+	}
 }
 
-function breathe(){
-    if (diam > 400) {
-        inOut = false;
-        breath_counter = breath_counter + 1;
-    }
-
-    if (diam < 50) {
-        inOut = true;
-        breath_counter = breath_counter + 1;
-    }
-    fill(110, 161, 164);
-    if (inOut) {
-        textFont(sarinaFont);
-        text("Breathe In", width / 2, height - 80);
-        diam++
-    }
-
-    if (!inOut) {
-        textFont(sarinaFont);
-        text("Breathe Out", width / 2, height - 80);
-        diam--
-    }
-    textSize(20);
-    textFont('Georgia');
-    if (num_breath_slider.value() - breath_counter == 0){
-        noLoop();
-        }
-    text(
-      "Breaths remaining: " + (num_breath_slider.value() - breath_counter), 
-      width / 2, height - 30
-    );
+function drawGradient() {
+	for (var r = 0; r < currentDiameter; ++r) {
+		var alphaFactor = 1.0 - (r / diameter);
+		fill(157, 183, 184, alphaFactor * 255.0);
+		ellipse(diameter / 2, diameter / 2, r);
+	}
 }
 
-function mouseClicked(event) {
-  start = 1;
+function drawChakra() {
+	tint(255, 63);
+	var width = diameter * chakraProportion;
+	var height = diameter * chakraProportion;
+	var x = (diameter - width) / 2;
+	var y = (diameter - height) / 2;
+	image(chakraImage, x, y, width, height)
 }
+
+function drawText() {
+	textFont(sarinaFont);
+	textAlign(CENTER, CENTER);
+	noStroke();
+	fill(255, 255, 255);
+	if (!running) {
+		textSize(32);
+		text('Start', diameter / 2, diameter / 2)
+	}
+	else {
+		textSize(24);
+		if (breathingIn)
+			text('breath in', diameter / 2, diameter / 2)
+		else
+			text('breath out', diameter / 2, diameter / 2)
+	}
+}
+
+function breathe() {
+	var secAngle = 360 / (animLength % 360);
+	var archSize = (getCurrentSec() - startedAt) * secAngle;
+	var sw = map(currentDiameter, 0, diameter, 1, 4);
+	strokeWeight(sw);
+	stroke(255, 204, 0);
+	noFill();
+	arc(diameter / 2, diameter / 2, currentDiameter-(sw+1), currentDiameter-(sw+1), midnight, midnight + archSize * (Math.PI/180));
+
+	if (currentDiameter > diameter) {
+		breathingIn = false;
+	}
+
+	if (currentDiameter < (diameter * minDiameterProportion)) {
+		breathingIn = true;
+		breathCount++;
+	}
+
+	if (breathingIn) currentDiameter++
+	else if (!breathingIn) currentDiameter--
+
+	if (getCurrentSec() >= startedAt+animLength) {
+		startedAt = 0;
+		currentDiameter = diameter = animWidth;
+		running = false;
+	}
+}
+
+function getCurrentSec() {
+	return new Date().getTime() / 1000;
+}
+
+document.getElementById('breathe').addEventListener('click', function() {
+	running = true;
+	currentDiameter = diameter * minDiameterProportion;
+	startedAt = getCurrentSec();
+	animLength = 60;
+})
--- public/js/breathe.js
+++ public/js/breathe.js
@@ -9,80 +9,109 @@ var minDiameterProportion = 0.05;
 var breathingIn = true;
 var breathCount = 0;
 
+var startedAt = 0;
+var animLength = 0;
+var animWidth = 0;
+var midnight = Math.PI*1.5;
+
 function preload() {
-    sarinaFont = loadFont('fonts/Sarina-Regular.ttf');
-    chakraImage = loadImage('img/logo-chakra.png')
+	sarinaFont = loadFont('fonts/Sarina-Regular.ttf');
+	chakraImage = loadImage('img/logo-chakra.png')
 }
 
 function setup() {
-    var parent = document.getElementById('breathe')
-    var myCanvas = createCanvas(parent.offsetWidth, parent.offsetHeight)
-    myCanvas.parent(parent.id);
-    currentDiameter = diameter = parent.offsetWidth;
+	var parent = document.getElementById('breathe')
+	var myCanvas = createCanvas(parent.offsetWidth, parent.offsetHeight)
+	myCanvas.parent(parent.id);
+	animWidth = parent.offsetWidth;
+	currentDiameter = diameter = animWidth;
 }
 
 function draw() {
-    clear();
-    noStroke();
+	clear();
+	noStroke();
 
-    drawGradient()
-    drawChakra();
-    drawText();
+	drawGradient()
+	drawChakra();
+	drawText();
 
-    if (running)
-        breathe();
+	if (running) {
+		breathe();
+	}
 }
 
 function drawGradient() {
-    for (var r = 0; r < currentDiameter; ++r) {
-        var alphaFactor = 1.0 - (r / diameter);
-        fill(157, 183, 184, alphaFactor * 255.0);
-        ellipse(diameter / 2, diameter / 2, r);
-    }
+	for (var r = 0; r < currentDiameter; ++r) {
+		var alphaFactor = 1.0 - (r / diameter);
+		fill(157, 183, 184, alphaFactor * 255.0);
+		ellipse(diameter / 2, diameter / 2, r);
+	}
 }
 
 function drawChakra() {
-    tint(255, 63);
-    var width = diameter * chakraProportion;
-    var height = diameter * chakraProportion;
-    var x = (diameter - width) / 2;
-    var y = (diameter - height) / 2;
-    image(chakraImage, x, y, width, height)
+	tint(255, 63);
+	var width = diameter * chakraProportion;
+	var height = diameter * chakraProportion;
+	var x = (diameter - width) / 2;
+	var y = (diameter - height) / 2;
+	image(chakraImage, x, y, width, height)
 }
 
 function drawText() {
-    textFont(sarinaFont);
-    textAlign(CENTER, CENTER);
-    noStroke();
-    fill(255, 255, 255);
-    if (!running) {
-        textSize(32);
-        text('Start', diameter / 2, diameter / 2)
-    }
-    else {
-        textSize(24);
-        if (breathingIn)
-            text('breath in', diameter / 2, diameter / 2)
-        else
-            text('breath out', diameter / 2, diameter / 2)
-    }
+	textFont(sarinaFont);
+	textAlign(CENTER, CENTER);
+	noStroke();
+	fill(255, 255, 255);
+	if (!running) {
+		textSize(32);
+		text('Start', diameter / 2, diameter / 2)
+	}
+	else {
+		textSize(24);
+		if (breathingIn)
+			text('breath in', diameter / 2, diameter / 2)
+		else
+			text('breath out', diameter / 2, diameter / 2)
+	}
 }
 
 function breathe() {
-    if (currentDiameter > diameter) {
-        breathingIn = false;
-    }
+	var secAngle = 360 / (animLength % 360);
+	var archSize = (getCurrentSec() - startedAt) * secAngle;
+	var sw = map(currentDiameter, 0, diameter, 1, 4);
+	strokeWeight(sw);
+	stroke(255, 204, 0);
+	noFill();
+	arc(diameter / 2, diameter / 2, currentDiameter-(sw+1), currentDiameter-(sw+1), midnight, midnight + archSize * (Math.PI/180));
 
-    if (currentDiameter < (diameter * minDiameterProportion)) {
-        breathingIn = true;
-        breathCount++;
-    }
+	if (currentDiameter > diameter) {
+		breathingIn = false;
+	}
 
-    if (breathingIn) currentDiameter++
-    else if (!breathingIn) currentDiameter--
+	if (currentDiameter < (diameter * minDiameterProportion)) {
+		breathingIn = true;
+		breathCount++;
+	}
+
+	if (breathingIn) currentDiameter++
+	else if (!breathingIn) currentDiameter--
+
+	if (getCurrentSec() >= startedAt+animLength) {
+		startedAt = 0;
+		currentDiameter = diameter = animWidth;
+		running = false;
+	}
 }
 
-document.getElementById('breathe').addEventListener('click', function () {
-    running = true;
-    currentDiameter = diameter * minDiameterProportion;
+function getCurrentSec() {
+	return new Date().getTime() / 1000;
+}
+
+document.getElementById('breathe').addEventListener('click', function() {
+	if (!running) {
+		running = true;
+		currentDiameter = diameter * minDiameterProportion;
+		startedAt = getCurrentSec();
+		animLength = 60;
+	}
 })
