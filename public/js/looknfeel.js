4550149f2c87f6f268e23504fa59b810d56d5e2c
Hudson Mendes <hudsonmendes@users.noreply.github.com>
2021-08-25 23:22:40 +0100

    Cosmetic changes (#5)
    * timer buttons adjusted
    
    * img compression (faster), better theming

--- public/js/looknfeel.js
+++ public/js/looknfeel.js
@@ -17,47 +17,57 @@ let explanationText = document.querySelector("#explanation");
 
 button1.addEventListener('click', () =>{
     console.log("button1 clicked");
-    document.body.style.background = "url('lnf/look-feel-1/sean-oulashin-KMn4VEeEPR8-unsplash.jpeg') no-repeat center center fixed";
+    document.body.style.background = "url('img/sean-oulashin-KMn4VEeEPR8-unsplash.jpeg') no-repeat center center fixed";
     document.body.style.backgroundSize = "cover";
     logoText.style.color = "#EDEBEC";
     explanationText.style.color = "#EDEBEC";
     textColour=[0xED,0xEB,0xEC];
+    outlineColour=[0xED,0xEB,0xEC];
+    breathingAnimationColour=[0x9D,0xB7,0xB8];
 });
 
 button2.addEventListener('click', () =>{
     console.log("button2 clicked");
-    document.body.style.background = "url('lnf/look-feel-1/elizeu-dias-RN6ts8IZ4_0-unsplash.jpg') no-repeat center center fixed";
+    document.body.style.background = "url('img/elizeu-dias-RN6ts8IZ4_0-unsplash.jpg') no-repeat center center fixed";
     document.body.style.backgroundSize = "cover";
-    logoText.style.color = "#056B06";
-    explanationText.style.color = "#056B06";
-    textColour=[0x05,0x6B,0x06];
+    logoText.style.color = "#ffffff";
+    explanationText.style.color = "#ffffff";
+    textColour=[0xff,0xff,0xff];
+    outlineColour=[0xff,0xff,0xff];
+    breathingAnimationColour=[0xb1, 0x99, 0x39];
 });
 
 button3.addEventListener('click', () =>{
     console.log("button3 clicked");
-    document.body.style.background = "url('lnf/look-feel-1/katerina-kerdi-yt1-v0TrOi8-unsplash.jpg') no-repeat center center fixed";
+    document.body.style.background = "url('img/katerina-kerdi-yt1-v0TrOi8-unsplash.jpg') no-repeat center center fixed";
     document.body.style.backgroundSize = "cover";
     logoText.style.color = "#ECEAEB";
     explanationText.style.color = "#ECEAEB";
     textColour=[0xEC,0xEA,0xEB];
+    outlineColour=[0xED,0xEB,0xEC];
+    breathingAnimationColour=[0x13, 0x56, 0x80];
 });
 
 button4.addEventListener('click', () =>{
     console.log("button4 clicked");
-    document.body.style.background = "url('lnf/look-feel-1/yang-wewe-H869_y8UJoI-unsplash.jpg') no-repeat center center fixed";
+    document.body.style.background = "url('img/yang-wewe-H869_y8UJoI-unsplash.jpg') no-repeat center center fixed";
     document.body.style.backgroundSize = "cover";
     logoText.style.color = "#E9EBE8";
     explanationText.style.color = "#E9EBE8";
     textColour=[0xE9,0xEB,0xE8];
+    outlineColour=[0xED,0xEB,0xEC];
+    breathingAnimationColour=[0x1c,0x44,0x89];
 });
 
 button5.addEventListener('click', () =>{
     console.log("button5 clicked");
-    document.body.style.background = "url('lnf/look-feel-1/anastasia-taioglou-EEDLURXCpqg-unsplash.jpg') no-repeat center center fixed";
+    document.body.style.background = "url('img/anastasia-taioglou-EEDLURXCpqg-unsplash.jpg') no-repeat center center fixed";
     document.body.style.backgroundSize = "cover";
     logoText.style.color = "#FFFFFF";
     explanationText.style.color = "#FFFFFF";
     textColour=[0xFF,0xFF,0xFF];
+    outlineColour=[0xED,0xEB,0xEC];
+    breathingAnimationColour=[0x17,0x05,0x39];
 });
 
 
